8cb8211a0ce7a418cb32d0d11836c3f9066c9ac2
dmswnlee <dlrmawnl03@gmail.com>
2023-12-31 17:12:30 +0900

    프로젝트 생성 및 초기 설정

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Todo from './pages/Todo';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+function App() {
+   return (
+      <>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/todo" element={<Todo />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+         </Routes>
+      </>
+   );
+}
+
+export default App;
