abb24b2d44b7812676a853e7d31414c4ff8181b9
dmswnlee <dlrmawnl03@gmail.com>
2024-01-01 23:55:54 +0900

    home 컴포넌트 기능 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import imgURL from '../assets/logo.svg';
 
 export default function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  );
+   return (
+      <div className="home">
+         <h1 className="logo">
+            <img src={imgURL} alt="logo" />
+         </h1>
+         <div className="search-bar">
+            <span className="icon material-symbols-outlined">search</span>
+            <p>Google List에 오신 걸 환영합니다!</p>
+         </div>
+      </div>
+   );
 }
 
