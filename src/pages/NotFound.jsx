7fe86cc79e39e971c6bb8a2026c02d692062e0fd
dmswnlee <dlrmawnl03@gmail.com>
2024-01-02 12:41:38 +0900

    NotFound 내용 추가

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export default function NotFound() {
-  return (
-    <div>
-      NotFound
-    </div>
-  );
+   return (
+      <div className="not-found">
+         <h1>
+            4<span class="material-symbols-outlined">sentiment_dissatisfied</span>4
+         </h1>
+         <p>원하시는 페이지를 찾을 수 없습니다.</p>
+         <p>입력하신 페이지의 주소가 맞는지 다시 한번 확인해 주세요.</p>
+      </div>
+   );
 }
-
