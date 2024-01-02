4680714781395f9a79063c08660f56be1082883e
dmswnlee <dlrmawnl03@gmail.com>
2024-01-02 12:20:30 +0900

    about 내용 및 이미지 추가

---
+++ src/data/about.js
@@ -0,0 +1,6 @@
+export const about = {
+   name: '이은주 / LeeEunJoo',
+   email: 'dlrmawnl03@gmail.com',
+   blog: 'https://jaydl.tistory.com/',
+   github: 'https://github.com/dmswnlee',
+};
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import profile_photo from '../assets/profile_ photo.png';
+import { about } from '../data/about';
 
 export default function About() {
-  return (
-    <div>
-      About
-    </div>
-  );
-}
+   const { name, email, blog, github } = about;
 
+   return (
+      <div className="about">
+         <div className="profile">
+            <div className="profile-photo">
+               <img src={profile_photo} alt="개발자의 사진" />
+            </div>
+            <div className="profile-content">
+               <p className="name">{name}</p>
+               <p>
+                  <a href={`mailto:${email}`} target="_blank">
+                     email
+                  </a>
+               </p>
+               <p>
+                  <a href={blog} target="_blank">
+                     blog
+                  </a>
+               </p>
+               <p>
+                  <a href={github} target="_blank">
+                     github
+                  </a>
+               </p>
+            </div>
+         </div>
+      </div>
+   );
+}
