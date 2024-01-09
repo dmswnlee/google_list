78a1b5bbe66651ffc2b016f5b7eb36bd463f52c4
dmswnlee <dlrmawnl03@gmail.com>
2024-01-09 14:25:52 +0900

    home 문구 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
             <img src={logo} alt="logo" />
          </h1>
          <p className='info'>
-            구글 리스트는 오늘의 할 일을 정리할 수 있는 투두앱 입니다. <br />
+            구글 리스트는 할 일을 정리할 수 있는 투두앱 입니다. <br />
             완료된 투두, 해야 할 투두를 확인할 수 있습니다. <br />
             또한 내가 작성한 투두를 검색 기능을 통해 확인할 수 있습니다.
          </p>
